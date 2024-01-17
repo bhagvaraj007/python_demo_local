31ec08f464d766ef69b35810d0161764e9895d00
bhagvaraj007 <sankalp24bhalerao@gmail.com>
2024-01-17 13:15:57 +0530

    HelloWorld added to track

---
+++ String_methods.py
@@ -0,0 +1,13 @@
+sentence ="hello this is String Practical"
+first_name="sankalp"
+last_name="bhalerao"
+#output = "Hello,{} {}".format(first_name,last_name)
+output = "Hello,{} {}".format(first_name,last_name)
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.count('e'))
+print(output)
+dob = 24
+print("My birthday comes on "+str(dob) + " April")
+
